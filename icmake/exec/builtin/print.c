dc5118c3ce48d69b7474ffa44a4f4ccff427244f
Martin Dorwig <martin.dorwig@gmail.com>
2015-10-16 18:51:50 +0200

    printf/fprintf call new function fnc_ffprintf which takes a FILE * and int start argument new formatstring witj %1 .. %n placeholder representing arg 1 .. arg n implicit typecast for list +/- string

--- icmake/exec/builtin/print.c
+++ icmake/exec/builtin/print.c
@@ -17,6 +17,79 @@
 
 #include "builtin.ih"
 
+static void printarg(FILE * out,size_t i,int * newelement)
+{
+  char * string = getarg(i,newelement);
+  fputs(string,out);
+  if (typeValue(top() - i) & e_list && *string && *newelement == 0)
+    putc(' ',out);
+  free(string);
+}
+
+void fun_printf (void)
+{
+  fun_ffprintf(stdout,1);
+}
+
+void fun_ffprintf (FILE * out,size_t start)
+{
+    int cprintf = 0;
+    size_t i;
+    int newelement;
+
+    size_t nargs = intValue(top());
+    char * fmt = getarg(start,&newelement);
+    char * cp = strchr(fmt,'%');
+    if (cp != NULL)
+    {
+      cprintf = isdigit(cp[1]);
+    }
+    if (cprintf)
+    {
+      char * last = fmt;
+      while(*fmt)
+      {
+        if (*fmt++ == '%')
+        {
+          if (isdigit(*fmt))
+          {
+            int l = fmt-1-last;
+            fwrite(last,1,l,out);
+            size_t i = strtol(fmt,&last,10) ; // parameter index
+            if (i >= 1 && start+i <= nargs)
+            {
+              do
+              {
+                printarg(out,start+i,&newelement);
+              } while(newelement == 0);
+            }
+            else
+            {
+              fprintf(out,"%%%u",(unsigned)i);
+            }
+          }
+          else if (*fmt == '%')
+          {
+            int l = fmt-last;
+            fwrite(last,1,l,out);
+            last = ++fmt;
+          }
+        }
+      }
+      if (last < fmt)
+      {
+        fwrite(last,1,fmt-last,out);
+      }
+    }
+    else
+    {
+      for (i = start; i <= nargs; i += newelement)
+      {
+          printarg(out,i,&newelement);
+      }
+    }
+}
+/* original
 void fun_printf ()
 {
     size_t i;
@@ -31,3 +104,4 @@ void fun_printf ()
         free(string);
     }
 }
+*/
